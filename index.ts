8167ca837019169b325a30d16f5ab95152c9d412
azu <azuciao@gmail.com>
2021-07-09 23:59:28 +0900

    init

---
+++ index.ts
@@ -0,0 +1,8 @@
+export type RequestBody =
+  | {
+    template: string;
+  }
+  | {
+    template: string;
+    newFolderName: string;
+  };
---
+++ test.ts
@@ -0,0 +1,11 @@
+import Ajv from "ajv"
+import { RequestBody } from "."
+import schema from "./index.json"
+const ajv = new Ajv({ removeAdditional: true })
+const request: RequestBody = {
+  template: "<% name %>",
+  newFolderName: "new name",
+}
+const valid = ajv.validate(schema, request)
+if (!valid) console.log(ajv.errors)
+console.log("request", request)
